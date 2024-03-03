d7d045a7886d2673eda73f9218be446c78015e6f
Madiar Shakhman <infinitycorpkz@gmail.com>
2024-03-03 23:52:06 +0500

    draw table

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -11,6 +11,25 @@
               <th class="users__thTitle">Действие</th>
             </tr>
           </tbody>
+          <tbody>
+            <tr v-for="(user, index) in sortedUsersList" :key="index">
+              <td>{{ user.username }}</td>
+              <td>{{ user.roles[0] }}</td>
+              <td>
+                <div class="table__btn-cnt" v-if="user.roles[0] !== 'ADMIN'">
+                  <button class="table__edit-user btn" @click="editUser(user)">
+                    Edit
+                  </button>
+                  <button
+                    class="table__delete-user btn"
+                    @click="deleteUser(user, $event)"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
@@ -22,22 +41,71 @@
 export default {
   data() {
     return {
-      usersList: "",
+      usersList: { admin: {}, users: [] },
     };
   },
   methods: {
     async getUsersList() {
       try {
         const response = await this.$axios.get("users");
-        this.usersList = JSON.parse(JSON.stringify(response.data.adminData));
-        console.log(console.log(this.usersList));
+        this.usersList = response.data.adminData;
+        //console.log(this.usersList);
       } catch (error) {
         console.log(error);
       }
+    },
+    async deleteUser(user, event) {
+      console.log(user);
+      let a = event.target.closest("tr");
+      console.log(a);
     },
   },
   mounted() {
     this.getUsersList();
   },
+  computed: {
+    sortedUsersList() {
+      const users = [...this.usersList.users];
+
+      // Сортирую пользователей, чтобы админы были первыми
+      users.sort((a, b) => {
+        const roleA = a.roles[0];
+        const roleB = b.roles[0];
+
+        if (roleA === "ADMIN") return -1;
+        if (roleB === "ADMIN") return 1;
+
+        return 0;
+      });
+      return users;
+    },
+  },
 };
 </script>
+
+<style>
+.table__btn-cnt {
+  display: flex;
+  justify-content: space-around;
+}
+
+.btn {
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 13px;
+  border-radius: 6px;
+}
+
+.table__edit-user {
+  background-color: rgb(245, 160, 2);
+  color: #eeecec;
+  padding: 5px 11px;
+  margin-right: 5px;
+}
+
+.table__delete-user {
+  background-color: #be3333;
+  color: #eeecec;
+  padding: 5px 9px;
+}
+</style>
